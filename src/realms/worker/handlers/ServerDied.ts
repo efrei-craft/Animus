26a7722fa544d96f15dc5dc7ee4f4afb31ef1870
JiveOff <antoine@jiveoff.fr>
2023-02-28 11:14:23 +0100

    🔨 Remove container

--- src/realms/worker/handlers/ServerDied.ts
+++ src/realms/worker/handlers/ServerDied.ts
@@ -1,6 +1,7 @@
 import { DockerHookType, WorkerMethod } from "../types"
 import prisma from "../../../clients/Prisma"
 import RedisClient from "../../../clients/Redis"
+import docker from "../../../clients/Docker"
 
 export const method: WorkerMethod = {
   exec: async (arg: string) => {
@@ -16,6 +17,9 @@ export const method: WorkerMethod = {
       "removeServer",
       arg
     )
+
+    const container = await docker.getContainer(arg)
+    await container.remove()
   },
   meta: {
     queueType: "set",
