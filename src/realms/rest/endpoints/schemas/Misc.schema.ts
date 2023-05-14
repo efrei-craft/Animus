203aff97c879c212d6776293e22bea0e7c819215
Niilyx _ <nait.meddour.idir100@gmail.com>
2023-05-14 20:55:07 +0200

    Removed /misc/api_key endpoint

--- src/realms/rest/endpoints/schemas/Misc.schema.ts
+++ src/realms/rest/endpoints/schemas/Misc.schema.ts
@@ -1,31 +1,6 @@
-import { Static, Type } from "@sinclair/typebox"
+import { Type } from "@sinclair/typebox"
 import { FastifySchema } from "fastify"
-import ApiKeySchema from "../../schemas/ApiKey.schema"
-import {ApiScope} from "@prisma/client";
 
-// scopes must check if the value is in Object.keys(ApiScope)
-const CreateAPIKeyBodySchema = Type.Object({
-  key: Type.String(),
-  description: Type.Optional(Type.String()),
-  scopes: Type.Array(Type.String({ minLength: 1 }), { minItems: 1 })
-})
-
-export const CreateAPIKeySchema: FastifySchema = {
-  tags: ["misc"],
-  summary: "Creates a new API key - temporarily public",
-  security: [
-    {
-      apiKey: [ApiScope.MISC]
-    }
-  ],
-  operationId: "createAPIKey",
-  body: CreateAPIKeyBodySchema,
-  response: {
-    200: Type.Ref(ApiKeySchema)
-  }
-}
-
-export type CreateAPIKeyBodySchema = Static<typeof CreateAPIKeyBodySchema>
 
 export const HelloSchema: FastifySchema = {
   tags: ["misc"],
--- src/realms/rest/endpoints/services/Misc.service.ts
+++ src/realms/rest/endpoints/services/Misc.service.ts
@@ -1,41 +1,6 @@
 import { Service } from "fastify-decorators"
-import { ApiKey, ApiScope } from "@prisma/client"
-import prisma from "../../../../clients/Prisma"
-import { ApiError } from "../../helpers/Error"
 
 @Service()
 export default class MiscService {
-  async createAPIKey(
-    key: string,
-    description: string,
-    scopes: string[]
-  ): Promise<ApiKey> {
-    const existing = await prisma.apiKey.findFirst({
-      where: {
-        key
-      }
-    })
 
-    if (existing) {
-      throw new ApiError("api-key-already-exists", 409)
-    }
-
-    const existingScopes: ApiScope[] = []
-    for (const scope of scopes) {
-      if (!Object.values(ApiScope).includes(scope as ApiScope)) {
-        throw new ApiError("invalid-scope", 400)
-      }
-      existingScopes.push(ApiScope[scope])
-    }
-
-    return prisma.apiKey.create({
-      data: {
-        key,
-        description,
-        scopes: {
-          set: existingScopes
-        }
-      }
-    })
-  }
 }
