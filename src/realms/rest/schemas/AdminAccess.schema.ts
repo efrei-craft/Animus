3130e470a1b05f6521d2ed96894e80135b167aa7
JiveOff <antoine@jiveoff.fr>
2023-09-03 02:16:19 +0200

    Update authentik groups

--- src/realms/rest/schemas/AdminAccess.schema.ts
+++ src/realms/rest/schemas/AdminAccess.schema.ts
@@ -3,8 +3,12 @@ import ApiKeySchema from "./ApiKey.schema"
 
 export default Type.Object(
   {
-    name: Type.String({ description: "The name of the admin" }),
-    email: Type.String({ description: "The email of the admin" }),
+    name: Type.String(),
+    email: Type.String(),
+    nickname: Type.String(),
+    groups: Type.Array(Type.String(), {
+      description: "Authentik groups the admin is in"
+    }),
     apiKey: Type.Optional(Type.Ref(ApiKeySchema))
   },
   { $id: "AdminAccess", description: "The schema describing an Admin Access" }
